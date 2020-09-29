99f37971805ee56c308e81c69e799fa91b091470
Pascal P <lacsap_666@yahoo.fr>
2020-09-29 23:16:48 +1300

    affine-cipher

---
+++ affine-cipher/affine_cipher.py
@@ -0,0 +1,104 @@
+import functools
+import re
+from typing import Tuple
+
+M = 26
+
+L2IX = { k: v for k, v in zip(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], range(0, M)) }
+
+IX2L = { v: k for k, v in L2IX.items() }
+
+NON_ALPHA_REXP = r'[^a-zA-Z0-9]'
+
+GRP_SIZE = 5 + 1 # + 1 for space
+
+
+##
+## utilities
+##
+
+def _xgcd(a: int, b: int) -> Tuple[int, int, int]:
+    """
+    Extended GCD(a, b) == a * x + b * y == g
+
+    here b will be 26 (always), g is 1 thus find (x, y) relative integer (Z)
+    such that a (> 0) * x + 26 * y == 1
+
+    ref. https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+         https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
+    """
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        (q, a), b = divmod(b, a), a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+def _gcd(a: int, b: int) -> int:
+    # a > 0 and b > 0
+    a, b = (b, a) if a < b else (a, b)
+    if b == 0: return a
+    r = a
+    while r > 1:
+        r = a % b
+        a, b = b, r
+    return a if r == 0 else r
+
+def _is_coprime(a: int) -> bool:
+    """
+    are (a, M) coprime?
+    """
+    return _gcd(a, M) == 1
+
+def check_coprime(fn):
+    @functools.wraps(fn)
+    def wrapper_fn(*args, **kwargs):
+        _, a, _ = args
+        if not _is_coprime(a):
+            raise ValueError(f'a {a} and m {M} must be coprimes')
+        res = fn(*args, **kwargs)
+        return res
+
+    return wrapper_fn
+
+def grouping(s: str, ch: str) -> str:
+    l = len(s) + 1  # as ch is a latin char (actually a string)
+    return s + ch + ' ' if l % GRP_SIZE == 0 else s + ch
+
+##
+## API (public)
+##
+
+@check_coprime
+def encode(plain_text: str, a: int, b: int) -> str:
+    """
+    E(x) = (a * x + b) % M
+    """
+    def encode_fn(x):
+        return IX2L[(a * L2IX[x.lower()] + b) % M] if not ('0' <= x <= '9') else x
+
+    ary = map(encode_fn,
+              filter(lambda x: not re.match(NON_ALPHA_REXP, x),
+                     list(plain_text)))
+
+    return functools.reduce(lambda s, c: grouping(s, c),
+                            ary,
+                            ' ').strip()
+
+@check_coprime
+def decode(ciphered_text: str, a: int, b: int) -> str:
+    """
+    D(y) = a^-1(y - b) mod m, where y = E(x) and a^-1 is MMI of a
+
+    MMI == Modular Multiplicative Inverse, it can be found using the
+    extended Euclidean algorithm (xgcd)
+    """
+    inv_a = _xgcd(a, M)[1]
+
+    def decode_fn(x):
+        return IX2L[(inv_a * (L2IX[x.lower()] - b)) % M] if not ('0' <= x <= '9') else x
+
+    return ''.join(map(decode_fn,
+                       filter(lambda x: not re.match(NON_ALPHA_REXP, x),
+                              list(ciphered_text))))
---
+++ affine-cipher/affine_cipher_test.py
@@ -0,0 +1,130 @@
+import unittest
+
+from affine_cipher import decode, encode
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
+
+LongStr =  """
+Chor. Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross'd lovers take their life;
+Whose misadventur'd piteous overthrows
+Doth with their death bury their parents' strife,
+The fearful passage of their death-mark'd love,
+And the continuance of their parents' rage,
+ Which, but their children's end, naught could remove,
+Is now the two hours' traffic of our stage;
+The which if you with patient ears attend,
+What here shall miss, our toil shall strive to mend.
+"""
+
+class AffineCipherTest(unittest.TestCase):
+
+    ## Encode
+    def test_encode_yes(self):
+        self.assertEqual(encode("yes", 5, 7), "xbt")
+
+    def test_encode_test(self):
+        self.assertEqual(encode("Test", 5, 7), "ybty")
+
+    def test_encode_no(self):
+        self.assertEqual(encode("no", 15, 18), "fu")
+
+    def test_encode_omg(self):
+        self.assertEqual(encode("OMG", 21, 3), "lvz")
+
+    def test_encode_o_m_g(self):
+        self.assertEqual(encode("O M G", 25, 47), "hjp")
+
+    def test_encode_mindblowingly(self):
+        self.assertEqual(encode("mindblowingly", 11, 15), "rzcwa gnxzc dgt")
+
+    def test_encode_numbers(self):
+        self.assertEqual(
+            encode("Testing,1 2 3, testing.", 3, 4), "jqgjc rw123 jqgjc rw"
+        )
+
+    def test_encode_deep_thought(self):
+        self.assertEqual(encode("Truth is fiction.", 5, 17), "iynia fdqfb ifje")
+
+    def test_encode_deep_thought_nospc(self):
+        self.assertEqual(encode("Truthisfiction.", 5, 17), "iynia fdqfb ifje")
+
+    def test_encode_thought(self):
+        self.assertEqual(encode("Free Will is a delusion. Face it", 7, 17),
+                         "agttp vqqvn rmtqb nvlea rftvu")
+
+    def test_encode_all_the_letters(self):
+        self.assertEqual(
+            encode("The quick brown fox jumps over the lazy dog.", 17, 33),
+            "swxtj npvyk lruol iejdc blaxk swxmh qzglf",
+        )
+
+    def test_encode_lon_str(self):
+        self.assertEqual(
+            encode(LongStr, 17, 19),
+            """bixwe dxixv njixy snkxe ityzh jzgsz rgzel zgatz wmjwx gtdij wjdjy tlxvw nbjgj awxpt gbzjg erwvs rjkwj thexg jdpve zgldi jwjbz mzyky xxspt hjnbz mzyit gsnvg byjtg awxpa xweie ijate tyyxz gnxae ijnje dxaxj ntotz wxane twbwx nnsyx mjwne thjei jzwyz ajdix njpzn tsmjg evwso zejxv nxmjw eiwxd nsxei dzeie ijzws jteik vwlei jzwot wjgen newza jeija jtwav yotnn trjxa eijzw sjtei ptwhs yxmjt gseij bxgez gvtgb jxaei jzwot wjgen wtrjd izbik veeij zwbiz yswjg njgsg tvrie bxvys wjpxm jzngx deije dxixv wnewt aazbx axvwn etrje ijdiz bizal xvdze iotez jgejt wntee jgsdi teijw jnity ypznn xvwex zynit yynew zmjex pjgs"""
+        )
+
+    def test_encode_with_a_not_coprime_to_m(self):
+        with self.assertRaisesWithMessage(ValueError):
+            encode("This is a test.", 6, 17)
+
+
+    ## Decode
+    def test_decode_exercism(self):
+        self.assertEqual(decode("tytgn fjr", 3, 7), "exercism")
+
+    def test_decode_test(self):
+        self.assertEqual(decode("ybty", 5, 7), "test")
+
+    def test_decode_a_sentence(self):
+        self.assertEqual(
+            decode("qdwju nqcro muwhn odqun oppmd aunwd o", 19, 16),
+            "anobstacleisoftenasteppingstone",
+        )
+
+    def test_decode_numbers(self):
+        self.assertEqual(decode("odpoz ub123 odpoz ub", 25, 7), "testing123testing")
+
+    def test_decode_all_the_letters(self):
+        self.assertEqual(
+            decode("swxtj npvyk lruol iejdc blaxk swxmh qzglf", 17, 33),
+            "thequickbrownfoxjumpsoverthelazydog",
+        )
+
+    def test_decode_all_the_letters2(self):
+        self.assertEqual(
+            decode("kqlfd jzvgy tpaet icdhm rtwly kqlon ubstx", 19, 13),
+            "thequickbrownfoxjumpsoverthelazydog",
+        )
+
+    def test_decode_with_no_spaces_in_input(self):
+        self.assertEqual(
+            decode("swxtjnpvyklruoliejdcblaxkswxmhqzglf", 17, 33),
+            "thequickbrownfoxjumpsoverthelazydog",
+        )
+
+    def test_decode_with_too_many_spaces(self):
+        self.assertEqual(
+            decode("vszzm    cly   yd cg    qdp", 15, 16), "jollygreengiant"
+        )
+
+    def test_decode_with_a_not_coprime_to_m(self):
+        with self.assertRaisesWithMessage(ValueError):
+            decode("Test", 13, 5)
+
+    def test_decode_with_a_not_coprime_to_m_2(self):
+        with self.assertRaisesWithMessage(ValueError):
+            decode("ybty", 18, 13)
+
+    # Utility functions
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
+
+
+if __name__ == "__main__":
+    unittest.main()
