fb9b69e3bb5711add131dc86c60dafb3a504e101
Pascal P <lacsap_666@yahoo.fr>
2021-02-02 12:00:25 +1300

    two-bucket: solution

--- two-bucket/two_bucket.py
+++ two-bucket/two_bucket.py
@@ -24,6 +24,8 @@ Valid moves:
 """
 
 class Bucket:
+    VERB = False
+
     def __init__(self, cap):
         assert cap > 0
         self.cap = cap
@@ -33,7 +35,7 @@ class Bucket:
         if self.content == self.cap:
             print("bucket already full!")
             return self
-        print(f"\t\tFilling bucket: {self} -> {self.cap}")
+        if __class__.VERB: print(f"\t\tFilling bucket: {self} -> {self.cap}")
         self.content = self.cap
         return self
 
@@ -41,17 +43,17 @@ class Bucket:
         if self.content == 0:
             print("bucket already empty!")
             return self
-        print(f"\t\tEmptying bucket: {self} -> 0")
+        if __class__.VERB: print(f"\t\tEmptying bucket: {self} -> 0")
         self.content = 0
         return self
 
     def transfer(self, d_bucket):
         if self.is_full(d_bucket):
-            print("destination bucket already full!")
+            if __class__.VERB: print("destination bucket already full!")
             return (self, d_bucket)
 
         elif self.is_empty():
-            print("src bucket is empty!")
+            if __class__.VERB: print("src bucket is empty!")
             return (self, d_bucket)
         ##
         ## now: self.content < self.cap && d_bucket.content <= d_bucket.cap => transfer possible
@@ -64,7 +66,7 @@ class Bucket:
             # then update dst / src accordingly
         #
         # now  can_receive <= d_bucket.content
-        print(f"\t\tTransfer from bucket: {self} to {d_bucket} of {can_receive} unit(s)")
+        if __class__.VERB: print(f"\t\tTransfer from bucket: {self} to {d_bucket} of {can_receive} unit(s)")
         self.content -= can_receive
         d_bucket.content += can_receive
         return (self, d_bucket)
@@ -86,218 +88,144 @@ class Bucket:
         return f"c: {self.content}, cap: {self.cap}"
 
 
-class Problem:
-    All_Actions = {'transfer', 'emptying', 'filling'} ## set
+"""
+  2 bucket B (big) S (Small)
+  Starting with biggest bucket (B), transitions are:
 
+  FB Fill B
+  TB Transfer B -> S
+  ES Empty Small
+
+  Transitions are typically (starting from biggest):
+   1     2         3            4
+  FB -> TB -> is S full? yes:  ES  -> back to 2
+              is S full? no: back to 1
+
+
+  FS Fill $
+  TS Transfer S -> B
+  EB Empty Big
+
+  Transitions are typically (starting from smallest):
+   1     2         3           4
+  FS -> TS -> is B full? yes: EB -> back to 2
+              is B full? no: back to 1
+
+  is the problem possible?
+  - 1 <= goal < b1.cap + b2.cap
+  - pgcd(B, C) != 1 only multiples of PGCD reachable
+
+"""
+class Problem:
     def __init__(self, b1: Bucket, b2: Bucket, goal: int, start: str):
         assert start == "one" or start == "two", "start must be 'one' or 'two'"
         self.b1, self.b2 = b1, b2
+        assert 1 <=  goal <=  b1.cap + b2.cap, f"goal should satisfy: 0 <= {goal} <= {b1.cap + b2.cap}"
         self.goal, self.start = goal, start
         self.move = 0
-        #
-        self.last_action = None
-        self.b_action = None # the bucket (dst) onto which last action was performed
-        self.pending = None
-        self.visited = []    # keep track of visited state to avoid cycle
-        self.ix = 0
 
-    def solve(self, src, dst, action=None):
-        if self.last_action is None:
-            print("FIRST S O L V E ...")
-            # first action ever...
-            if self.start == "one":
-                self.filling(self.b1)
-                self.b_action = self.b1
-            else:
-                # self.start == "one"
-                self.filling(self.b2)
-                self.b_action = self.b2
-            #
-            self.last_action = "filling"
-            self.move = 1
-            return self.action()
+    def solve(self, src, dst):
+        gcd_ = gcd(self.b1.cap, self.b2.cap)
 
-        elif self.goal_reached():
-            print("================================= G O A L  R E A C H E D =========================")
-            return True
+        if gcd_ != 1 and self.goal % gcd_ != 0:
+            raise ValueError("goal {self.goal} is not reachable")
 
+        # trivially reachable goals
+        if self.goal == self.b1.cap:
+            return (1, self.start, 0)
+
+        elif self.goal == self.b2.cap:
+            return (1, self.start, 0)
+
+        elif self.goal == self.b1.cap + self.b2.cap:
+            return (2, self.start, self.b2.content + self.b1.contant)
+
+        if self.start == "one":
+            return self.b2_strategy()
         else:
-            # exec. next action
-            if action == "transfer":
-                src.transfer(dst)
-
-            elif action == "emptying":
-                dst.emptying()
-
-            else:
-                dst.filling()
-
-            print(f"S O L V E ... state (updated): {self.b1} // {self.b2}")                
-            self.last_action = action
-            self.b_action = dst
-            self.move += 1
-            return self.action()
-            
-    def action(self):
-        poss_actions = self.possible_actions()
-        print(f"A C T I O N ... possible actions are: {poss_actions} / move: {self.move} / state: <{self.b1}, {self.b2}>")
-        self.ix += 1
-        if self.ix == 10:
-            print("STOP...")
-            return False
-        
-        assert not(self.b1.is_empty() and self.b2.is_empty()), "both bucket cannot be empty - this was the init state!"        
-
-        for action in poss_actions:
-            state = {
-                "b1": copy(self.b1),
-                "b2": copy(self.b2),
-                "move": self.move,
-                "last_action": self.last_action
-            }
-            src, dst = None, None
-            
-            # do_next action, conditionally on previous action (self.last_action)
-            if self.b_action == self.b1:
-                src, dst, action = self.action_for(action, new_dst=self.b2)
-                if action is None: continue
-            ##
-            elif self.b_action == self.b2:
-                src, dst, action = self.action_for(action, new_dst=self.b1)
-                if action is None: continue
-
-            else:
-                assert self.b_action is not None, f"b_action {self.b_action} MUST be defined, got None"
-            
-            print(f"\tTRYING action {action}")    
-            while True:
-                self.solve(src, dst, action)
-                
-                if self.goal_reached():
-                    print(f"\tPOST RECURSION 2...")
-                    return True
-
-                v_state = {"src": src, "dst": dst, "action": action}
-                if not self.find_visited(v_state):
-                    self.visited.append(v_state)
-
-                # failure => backtrack
-                self.b1 = state["b1"]
-                self.b2 = state["b2"]
-                self.move = state["move"]
-                self.last_action = state["last_action"]
-
-                # is there a pending action
-                if self.pending is not None:
-                    src, dst = self.pending['new_src'], self.pending['new_dst']
-                    self.pending = None
-                    print(f">> PENDING action: {action} / src: {src} / dst: {dst}")
-                    ## Go back to solve(...)
-                else:
-                    break
-        ## TODO: try again inverting src and dst bucket !! HOW? / NOT REQUIRED...
-        return False
+            # self.start == "two"
+            return self.b1_strategy()
 
     def filling(self, b: Bucket):
         b.filling()
 
-    def possible_actions(self):
-        return list(__class__.All_Actions - set([self.last_action]))
-
     def solution(self):
-        # need to tell wich bucket reached the goal
-        gb = self.goal_for()
-        return (self.move, "one" if gb == self.b1 else "two", self.b2.content if gb == self.b1 else self.b1.content)
+        #                         remaining in other bucket
+        return (self.move, "one", self.b2.content) if self.b1.content == self.goal else \
+            (self.move, "two", self.b1.content)
 
     def goal_reached(self) -> bool:
-        return self.b1.content == self.goal or self.b2.content == self.goal
+        return self.b1.content == self.goal or self.b2.content == self.goal or \
+            self.b1.content + self.b2.content == self.goal
 
-    def goal_for(self):
-        return self.b1 if self.b1.content == self.goal else self.b2
+    def b1_strategy(self):
+        """
+        FS -> TS -> is N full? yes:  EB  -> back to 2
+                    is B full? no: back to 1
+        """
+        nextop = "filling"
+        while not self.goal_reached():
+            if nextop == "filling":
+                self.filling(self.b2)
+                self.move += 1
+                nextop = "transfer"
 
-    def find_visited(self, state) -> bool:
-        for _state in self.visited:
-            if _state.src != satte.src or _satte.dst != state.dst or \
-               _state.action != state.action:
-                return False
-        return True
+            if nextop == "transfer":
+                self.move += 1
+                self.b2.transfer(self.b1)
 
-    def action_for(self, action, new_dst):
-        src = self.b2 if new_dst == self.b1 else self.b1
-        if self.last_action == "filling":
-            if action == 'filling':
-                return (src, new_dst, action)  ## next (dst) bucket will be new_dst
+            if self.goal_reached(): break
 
-            elif action == 'emptying':
-                if new_dst.is_empty(): return (None, None, None) ## go straight to next available action
-                ##                                 ## because no point...
-                else: ## not new_dst.is_empty() => we can empty new_dst
-                    return (src, new_dst, action)  ## next (dst) bucket will be new_dst
-                    ## NOTE: now emptying bq is pointless as we just filled it
-
-            elif action == 'transfer':
-                ## 2 possibilities here:
-                ##   - transfer(b1 -> b2) if b1.content > 0 && b2.content < b2.cap
-                ##   - transfer(b2 -> b1) if b2.content > 0 && b1.content < b1.cap
-                ##
-                ## FIXME: Naming is not really good here...
-                if not src.is_empty() and not new_dst.is_full():
-                    self.pending = {'new_src': new_dst, 'new_dst': src}
-                    return (src, new_dst, action)
-                    #
-                elif not new_dst.is_empty() and not src.is_full():
-                    src, new_dst = new_dst, src    ## swap!
-                    self.pending = {'new_src': src, 'new_dst': new_dst}
-                    return (src, new_dst, action)
-                    #
-                else:
-                    return (None, None, None)
-
-        elif self.last_action == "transfer":  ## Ex. transfer b2(src: new_dst) -> b1 (dst: src)
-            if action == 'filling':
-                ## filling b2/new_dst is possible
-                self.pending = {'new_src': new_dst, 'new_dst': src}
-                return (src, new_dst, action)
-
-                ## filling b1/ is possible if b1 not full
-                src, new_dst = new_dst, src    ## swap!
-                return (src, new_dst, action)
-
-            elif action == 'emptying':
-                ## emptying b1/src is possible iff b1/src is not full
-                if not src.is_full() and not new_dst.is_empty():
-                    print(f"11111111111 / bucket: {src}")
-                    src, new_dst = new_dst, src    ## swap!
-                    self.pending = {'new_src': src, 'new_dst': new_dst}
-                    return (src, new_dst, action)
-
-                ## emptying b2 is possible iff b2 not empty and we do not end up with both bucket empty!
-                if not new_dst.is_empty():
-                    print("22222222222")
-                    return (src, new_dst, action)
-                else:
-                    return (None, None, None)
-
-            elif action == 'transfer':
-                return (None, None, None)     ## cannot do 2 transfer in a row
-
-        elif self.last_action == "emptying":  ## Ex. emptying b1
-            if action == 'filling':
-                ## filling b1 is possible iff b1 was not full when emptied? How to we know this?
-                ## by checking the (already) visited state
-                ##
-                self.pending = {'new_src': src, 'new_dst': new_dst}  ## filling b2 is possbile iff b2 is not full
-                src, new_dst = new_dst, src    ## swap!
-                return (src, new_dst, action)
-
-            elif action == 'emptying':
-                return (None, None, None)     ## NO-OP or return to init. state
-
-            elif action == 'transfer':
-                ## only possible from b2 -> b1 as we emptied b1
-                return (src, new_dst, action)
+            if self.b1.is_full():
+                self.b1.emptying()
+                self.move += 1
+                next_op = "transfer"
+            else:
+                nextop = "filling"
         #
-        return (None, None, None)
+        # print(f"\n\tFINAL STATE: <{self.b1} / {self.b2}> - start was: {self.start}")
+        return self.solution()
+
+    def b2_strategy(self):
+        """
+        FB -> TB -> is S full? yes:  ES  -> back to 2
+                    is S full? no: back to 1
+        """
+        nextop = "filling"
+        while not self.goal_reached():
+            if nextop == "filling":
+                self.filling(self.b1)
+                self.move += 1
+                nextop = "transfer"
+
+            if nextop == "transfer":
+                self.move += 1
+                self.b1.transfer(self.b2)
+
+            if self.goal_reached(): break
+
+            if self.b2.is_full():
+                self.b2.emptying()
+                self.move += 1
+                next_op = "transfer"
+            else:
+                nextop = "filling"
+        #
+        # print(f"\n\tFINAL STATE: <{self.b1} / {self.b2}> - start was: {self.start}")
+        return self.solution()
+
+def gcd(x: int, y: int) -> int:
+    """
+    assume x > 0 and y > 0
+    """
+    x, y = (y, x) if x < y else (x, y)
+    if y == 0: return x
+    r = x
+    while r > 1:
+        r = x % y
+        x, y = y, r
+    #
+    return x if r == 0 else r
 
 #
 def measure(bucket_one: int, bucket_two: int, goal: int, start_bucket: str):
@@ -306,16 +234,4 @@ def measure(bucket_one: int, bucket_two: int, goal: int, start_bucket: str):
     b1 = Bucket(bucket_one)
     b2 = Bucket(bucket_two)
     pb = Problem(b1, b2, goal, start_bucket)
-
-    if pb.solve(b1, b2) is not None:
-        return pb.solution()
-    else:
-        return None # no solution
-#
-# assert transfer(src_bucket=2, src_cap=3, dst_bucket=4, dst_cap=5) == (1, 5)
-# assert transfer(src_bucket=3, src_cap=3, dst_bucket=4, dst_cap=5) == (2, 5)
-# assert transfer(src_bucket=1, src_cap=3, dst_bucket=0, dst_cap=5) == (0, 1)
-# assert transfer(src_bucket=0, src_cap=3, dst_bucket=5, dst_cap=5) == (0, 5)
-# assert transfer(src_bucket=2, src_cap=3, dst_bucket=1, dst_cap=5) == (0, 3)
-# assert transfer(src_bucket=0, src_cap=3, dst_bucket=4, dst_cap=5) == (0, 4)
-#
+    return pb.solve(b1, b2)
--- two-bucket/two_bucket_test.py
+++ two-bucket/two_bucket_test.py
@@ -6,35 +6,73 @@ from two_bucket import measure
 
 
 class TwoBucketTest(unittest.TestCase):
-    def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_one(
+    def test_measure_using_b1_of_size_3_and_b2_of_size_5_start_with_b1_1(
         self
     ):
         self.assertEqual(measure(3, 5, 1, "one"), (4, "one", 5))
 
-    def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_two(
+    def test_measure_using_b1_of_size_5_and_b2_of_size_3_start_with_b1_2_4(
+        self
+    ):
+        # make 4L
+        self.assertEqual(measure(5, 3, 4, "one"), (6, "one", 3))
+        self.assertEqual(measure(5, 3, 4, "two"), (8, "one", 0))
+
+    def test_measure_using_b1_of_size_5_and_b2_of_size_3_start_with_b1_2_7(
+        self
+    ):
+        # make 7L
+        self.assertEqual(measure(5, 3, 7, "one"), (6, "two", 4))
+        self.assertEqual(measure(5, 3, 7, "two"), (10, "two", 5))
+
+    def test_measure_using_b1_of_size_3_and_b2_of_size_5_start_with_b2_1(
         self
     ):
         self.assertEqual(measure(3, 5, 1, "two"), (8, "two", 3))
 
-    def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_one(
+    def test_measure_using_b1_of_size_5_and_b2_of_size_3_start_with_b2_4(
+        self
+    ):
+        # make 4L
+        self.assertEqual(measure(5, 3, 4, "two"), (8, "one", 0))
+
+    def test_measure_using_b1_of_size_7_and_b2_of_size_11_start_with_b1(
         self
     ):
         self.assertEqual(measure(7, 11, 2, "one"), (14, "one", 11))
 
-    def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_two(
+    def test_measure_using_b1_of_size_7_and_b2_of_size_11_start_with_b2(
         self
     ):
         self.assertEqual(measure(7, 11, 2, "two"), (18, "two", 7))
 
-    def test_measure_one_step_using_bucket_one_of_size_1_and_bucket_two_of_size_3_start_with_bucket_two(
+    def test_measure1_step_using_b1_of_size_1_and_b2_of_size_3_start_with_b2(
         self
     ):
         self.assertEqual(measure(1, 3, 3, "two"), (1, "two", 0))
 
-    def test_measure_using_bucket_one_of_size_2_and_bucket_two_of_size_3_start_with_bucket_one_and_end_with_bucket_two(
+
+    def test_measure_using_b1_of_size_6_and_b2_of_size_15_start_with_b1_4_impossible(
         self
     ):
-        self.assertEqual(measure(2, 3, 3, "one"), (2, "two", 2))
+        with self.assertRaisesWithMessage(ValueError):
+            self.assertEqual(measure(6, 15, 4, "one"))
+
+    def test_measure_using_b1_of_size_6_and_b2_of_size_15_start_with_b1_1_2(
+        self
+    ):
+        self.assertEqual(measure(6, 15, 9, "one"), (10, "two", 0))
+        self.assertEqual(measure(6, 15, 9, "two"), (2, "two", 6))
+
+
+    # def test_measure_using_b1_of_size_2_and_b2_of_size_3_start_with_b1_and_end_with_b2(
+    #    self
+    #):
+    #    self.assertEqual(measure(2, 3, 3, "one"), (2, "two", 2))
+
+    # Utility functions
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 
 if __name__ == "__main__":
