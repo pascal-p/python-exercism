3e9a45e9577c346e0ff280b74993705c8359ed82
Pascal P <lacsap_666@yahoo.fr>
2021-05-17 21:40:51 +1200

    nth-prime: review

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,7 +1,7 @@
 from typing import List# , Tuple
 
-intList = List[int]
-boolList = List[bool]
+L_Int = List[int]
+L_Bool = List[bool]
 
 MAX_PRIME_LIM = 1_000_000
 
@@ -11,34 +11,34 @@ def prime(num: int, lim=MAX_PRIME_LIM) -> int:
     if not 0 < num < lim:
         raise ValueError("Not in acceptable interval")
 
-    _primes = gen_primes_lt(MAX_PRIME_LIM)
-    # _primes = list(gen_primes(MAX_PRIME_LIM))
+    _primes = gen_primes_lt(lim)
+    # alt:  _primes = list(gen_primes(MAX_PRIME_LIM))
     return _primes[num - 1]
 
 
-def gen_primes_lt(n: int) -> intList:
+def gen_primes_lt(n: int) -> L_Int:
     prime_ind = [True] + [
         True for _ in range(3, n - 1, 2)
-    ]
+    ]                                        # gen. list of candidate prime [3, 5, 7, ...]...
+    #                                        # ... a priori primes (set to True)
 
     ix, m = 0, len(prime_ind)
-
     while ix < m:
-        cp = 2 * ix + 3
-        for jx in range(ix + cp, m, cp):
+        cp = 2 * ix + 3                      # candidate prime cp from 3, 5, 7, 11...
+        for jx in range(ix + cp, m, cp):     # mark multiples of cp as non prime (sieve method)
             prime_ind[jx] = False
 
-        ix += 1
+        ix += 1                              # set ix to next (candidate) prime
         while ix < m and not prime_ind[ix]:
             ix += 1
     #
     return [2] + [
         2 * ix + 3 for ix in range(m) if prime_ind[ix]
-    ]
+    ]                                        # return computed list of prime adding 2 (the only even int to be prime)
 
 
 #
-# Alt. with generator
+# Alt. an attempt with generator
 #
 def gen_primes(n:int):
     ix = 0
@@ -55,9 +55,9 @@ def gen_primes(n:int):
 
 def gen_primes_hlpr(n: int):
     """
-     3  5  7  9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51
-     T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T
-     0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    cp    3  5  7  9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51
+    flag  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T  T
+    ix    0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
     """
     primes = [2]
     prime_ind = [True] + [
@@ -65,7 +65,7 @@ def gen_primes_hlpr(n: int):
     ]
     return (primes, prime_ind)
 
-def gen_next_primes(ix:int, primes:intList, prime_ind:boolList) -> int:
+def gen_next_primes(ix:int, primes:L_Int, prime_ind:L_Bool) -> int:
     if ix >= len(prime_ind):
         return None
 
--- nth-prime/nth_prime_test.py
+++ nth-prime/nth_prime_test.py
@@ -29,12 +29,14 @@ class NthPrimeTest(unittest.TestCase):
     def test_big_prime(self):
         self.assertEqual(prime(10001), 104743)
 
+    def test_bigger_prime(self):
+        self.assertEqual(prime(100_001, lim=10_000_000), 1299721)
+
     def test_there_is_no_zeroth_prime(self):
         with self.assertRaisesWithMessage(ValueError):
             prime(0)
 
     # Additional tests for this track
-
     def test_first_twenty_primes(self):
         self.assertEqual(
             prime_range(20),
@@ -61,6 +63,24 @@ class NthPrimeTest(unittest.TestCase):
                 71,
             ],
         )
+
+    def test_first_hundred_primes(self):
+        self.assertEqual(
+            prime_range(100),
+            [
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+                127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+                233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+                283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+                353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+                419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+                467, 479, 487, 491, 499, 503, 509, 521, 523, 541
+            ],
+        )
+
 
     # Utility functions
     def assertRaisesWithMessage(self, exception):
