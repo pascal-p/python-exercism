71dfae8c9318d101362d6e1ac944be5b34cc43e1
Pascal P <lacsap_666@yahoo.fr>
2022-02-16 22:25:49 +1300

    darts: complete

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,21 @@
-def score(x, y):
-    pass
+"""
+    Darts Exo
+"""
+
+import math
+
+
+def score(x: float, y: float) -> int:
+    d = euclidean_dist(x, y)
+    if d > 10.0:
+        return 0
+    elif d > 5.0:
+        return 1
+    elif d > 1.0:
+        return 5
+    else:
+        return 10
+
+
+def euclidean_dist(x: float, y: float) -> float:
+    return math.sqrt(x ** 2 + y ** 2)
